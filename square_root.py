30300d5702a7577c5237da8a250b560d64959d5a
Ravi Varma Injeti <rvarmainjeti@gmail.com>
2020-09-02 06:23:09 +0530

    square_root

---
+++ square_root.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 06:17:33 2020
+
+@author: Ravi Varma Injeti
+"""
+import math
+
+def square_root(x):
+    sqr = math.sqrt(x)
+    return int(sqr)
+
+
+
+
+print(square_root(8))
